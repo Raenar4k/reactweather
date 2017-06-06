c5174c2da3ca261fa86d9d91eba4eb818d8a8253
raenar4k <raenardev@gmail.com>
2017-06-06 23:45:09 +0200

    Add simple call to autocomplete city widget on button click

---
+++ app/src/main/java/com/github/raenar4k/reactweather/Constants.kt
@@ -0,0 +1,3 @@
+package com.github.raenar4k.reactweather
+
+const val REQUEST_CODE_PLACE_AUTOCOMPLETE: Int = 1
--- app/src/main/java/com/github/raenar4k/reactweather/ui/main/MainActivity.kt
+++ app/src/main/java/com/github/raenar4k/reactweather/ui/main/MainActivity.kt
@@ -1,9 +1,18 @@
 package com.github.raenar4k.reactweather.ui.main
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
 import com.github.raenar4k.reactweather.R
+import com.github.raenar4k.reactweather.REQUEST_CODE_PLACE_AUTOCOMPLETE
 import com.github.raenar4k.reactweather.getApplication
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException
+import com.google.android.gms.common.GooglePlayServicesRepairableException
+import com.google.android.gms.location.places.AutocompleteFilter
+import com.google.android.gms.location.places.AutocompleteFilter.TYPE_FILTER_CITIES
+import com.google.android.gms.location.places.ui.PlaceAutocomplete
 import io.reactivex.android.schedulers.AndroidSchedulers
 import timber.log.Timber
 
@@ -13,6 +22,9 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
     getUsers()
+
+    val button = findViewById(R.id.button) as Button
+    button.setOnClickListener { startPlaceAutocomplete() }
   }
 
   private fun getUsers() {
@@ -24,5 +36,28 @@ class MainActivity : AppCompatActivity() {
         { Timber.d("Success") },
         { Timber.d("Error") }
       )
+  }
+
+  private fun startPlaceAutocomplete() {
+    try {
+      val intent = PlaceAutocomplete
+        .IntentBuilder(PlaceAutocomplete.MODE_OVERLAY)
+        .setFilter(AutocompleteFilter.Builder().setTypeFilter(TYPE_FILTER_CITIES).build())
+        .build(this)
+      startActivityForResult(intent, REQUEST_CODE_PLACE_AUTOCOMPLETE)
+    } catch (e: GooglePlayServicesRepairableException) {
+      Timber.d(e)
+    } catch (e: GooglePlayServicesNotAvailableException) {
+      Timber.d(e)
+    }
+  }
+
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    super.onActivityResult(requestCode, resultCode, data)
+
+    if (requestCode == REQUEST_CODE_PLACE_AUTOCOMPLETE && resultCode == Activity.RESULT_OK) {
+      val place = PlaceAutocomplete.getPlace(this, data)
+      Timber.d("Selected city: ${place.name}, id: ${place.id}")
+    }
   }
 }
